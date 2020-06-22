1ceaa52260615f358fce5da6d37334ed3656991f
Victoria Loja <victorialoja72@gmail.com>
2020-06-21 19:47:41 -0500

    Comit inicial

---
+++ edad.py
@@ -0,0 +1,12 @@
+#codigo de ejemplo para github
+#Autor: Victoria Loja <victorialoja72@gmail.com>
+#fecha: 22-06-2020
+#version: 0.0.1
+#
+#Calcula tu edad el año actual
+anio_actual = 2020
+nombre = input("Cual es tu nombre: ")
+anio_nacimiento = int(input("En que año naciste: "))
+
+edad = anio_actual - anio_nacimiento
+print("\nHola", nombre, "Tu edad en este año es: ", edad);
